5e9d8a6b1fc91bea856a80b53349726f6905eb4e
GiaKiet99 <kietdg99@gmail.com>
2021-08-12 23:42:06 +0700

    Output Management: multi bundle

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import _ from "lodash";
-import "./style.css";
-import Heart from "./heart.gif";
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Heart;
-
-  element.appendChild(myIcon);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js"
+  },
+  output: {
+    filename: "[name].bundle.js", // filename base on entry point
+    path: path.resolve(__dirname, "dist")
+  }
+}
 
 /**
  * Configuration type: function
@@ -22,24 +35,3 @@ const path = require("path");
 //     }
 //   }
 // }
-
-module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
-    ]
-  }
-}
