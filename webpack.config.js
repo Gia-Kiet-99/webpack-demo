63cb67f0795a3226b953d0ba5c2801610ad2c994
GiaKiet99 <kietdg99@gmail.com>
2021-08-13 22:35:43 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    another: './src/another-module.js',
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared',
+    },
+    another: {
+      import: './src/another-module.js',
+      dependOn: 'shared',
+    },
+    shared: 'lodash',
   },
   // using source map in order to map compiled code back to original source code 
   // to tracking errors, warnings
@@ -28,7 +35,10 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true, // clean dist folder before each build
     // publicPath: '/',
-  }
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 }
 
 
